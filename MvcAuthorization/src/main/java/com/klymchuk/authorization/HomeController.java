792e1eddb52a21ae91624d6ca7cbfeedd993df38
iklymchuk <klimchuk_ivan@mail.ru>
2015-01-27 18:18:18 +0200

    add new security configs

--- MvcAuthorization/src/main/java/com/klymchuk/authorization/HomeController.java
+++ MvcAuthorization/src/main/java/com/klymchuk/authorization/HomeController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -20,13 +19,19 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView login () {
-		return new ModelAndView ("login", "user", new User());
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
 	}
-	 */
+
 }
 
